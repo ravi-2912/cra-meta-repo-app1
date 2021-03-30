bf1708b879b58af97a548d296ce8a08e43d33437
Ravinder Singh <ravi_29_12@hotmail.com>
2021-03-30 22:23:35 +0100

    Updated override to get all packages inside common/ & add to babel loader include

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,27 @@
 // config-overrides.js
 // see: https://github.com/timarney/react-app-rewired
 
-const path = require("path");
 const fs = require("fs");
-
+const path = require("path");
 const rewireBabelLoader = require("react-app-rewire-babel-loader");
 
 const appDirectory = fs.realpathSync(process.cwd());
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
 
 module.exports = function override(config, env) {
-    config = rewireBabelLoader.include(
-        config,
-        resolveApp("../../common/cra-meta-repo-ui1")
-    );
+    const commonFolder = "../../common";
+
+    const getDirectories = (source) =>
+        fs
+            .readdirSync(source, { withFileTypes: true })
+            .filter((dirent) => dirent.isDirectory())
+            .map((dirent) => `${commonFolder}/${dirent.name}`);
+
+    const commonDirectories = getDirectories(commonFolder);
+
+    commonDirectories.forEach((dir) => {
+        config = rewireBabelLoader.include(config, resolveApp(dir));
+    });
 
     return config;
 };
