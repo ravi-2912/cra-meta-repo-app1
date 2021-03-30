7b245647e6426550fc983748123eebbae354a965
Ravinder Singh <ravi_29_12@hotmail.com>
2021-03-30 21:09:00 +0100

    Added react-app-rewired config-overrides.js file

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+// config-overrides.js
+// see: https://github.com/timarney/react-app-rewired
+
+const path = require("path");
+const fs = require("fs");
+
+const rewireBabelLoader = require("react-app-rewire-babel-loader");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+module.exports = function override(config, env) {
+    config = rewireBabelLoader.include(
+        config,
+        resolveApp("../../common/cra-meta-repo-ui1")
+    );
+
+    return config;
+};
